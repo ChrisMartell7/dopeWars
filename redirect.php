7522478c199bc789ac852b36d41cfe21b8fc8adf
root <root@app1.(none)>
2010-11-30 05:07:48 +0000

    added redirect

---
+++ redirect.php
@@ -0,0 +1,5 @@
+<script type="text/javascript">
+<!--
+window.location = "https://graph.facebook.com/oauth/authorize?client_id=110042632398450&redirect_uri=http://173.203.90.74/dopeWars/&type=user_agent&display=popup"
+//-->
+</script>
