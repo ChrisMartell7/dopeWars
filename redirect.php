5944ad0e972cff22de62f6ae0d10fc3e09c30fa5
root <root@app1.(none)>
2010-12-02 05:15:33 +0000

    working

--- redirect.php
+++ redirect.php
@@ -1,5 +1,3 @@
 <script type="text/javascript">
-<!--
-window.location = "https://graph.facebook.com/oauth/authorize?client_id=110042632398450&redirect_uri=http://173.203.90.74/dopeWars/&type=user_agent&display=popup"
-//-->
+window.location="http://www.facebook.com/connect/uiserver.php?app_id=110042632398450&method=permissions.request&display=popup&next=http%3A%2F%2F173.203.90.74%2FdopeWars%2Fredirect2.php%3F&type=user_agent&fbconnect=1"
 </script>
